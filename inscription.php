5bc654ba535111047340daaae79a7f3e47803c92
Borel <ntiakoumbaborelgiovanni@gmail.com>
2022-10-18 08:41:13 -0400

    control de saisi commit

--- inscription.php
+++ inscription.php
@@ -39,18 +39,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $passwordConfirmation = trim($_POST['password-confirmation']);
         
         $user = new Users();
-
+        //die(var_dump($birthday));
         if (empty($fname)) {
-            $nameErr = "* Le nom est obligatoire";
+            $fnameErr = "* Le nom est obligatoire";
         } else {
-            $name = cleanUpInputs($name);
+            $fname = cleanUpInputs($fname);
             // check if name only contains letters and whitespace
-            if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-                $nameErr = "* Seules les lettres et les espaces blancs sont autorisés";
+            if (!preg_match("/^[a-zA-Z-' ]*$/",$fname)) {
+                $fnameErr = "* Seules les lettres et les espaces blancs sont autorisés";
             }
-            if (strlen($name) > 100) {
-                $nameErr = "* Le nom doit comporter un maximum de 100 caractères.";
+            if (strlen($fname) > 100) {
+                $fnameErr = "* Le nom doit comporter un maximum de 100 caractères.";
             }
+        }if (empty($lname)) {
+            $lnameErr = "* Le prenom est obligatoire";
+        } else {
+            $lname = cleanUpInputs($lname);
+            // check if name only contains letters and whitespace
+            if (!preg_match("/^[a-zA-Z-' ]*$/",$lname)) {
+                $lnameErr = "* Seules les lettres et les espaces blancs sont autorisés";
+            }
+            if (strlen($lname) > 100) {
+                $lnameErr = "* Le nom doit comporter un maximum de 100 caractères.";
+            }
+        }
+        if (empty($birthday)) {
+            $birthdayErr = "* La date de naissance est obligatoire";
+        } else {
+            $date = '2012-01-01';
+            //die(var_dump($date < $birthday));
+            if( $date < $birthday){
+                 $birthdayErr = "*la date est trop recente";
+                 
+            }
+        }
+        if (empty($address)) {
+            $addressErr = "* l'addresse est obligatoire";
         }
         if (empty($username)) {
             $usernameErr = "* Le nom d'usager est obligatoire";
@@ -93,7 +117,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $message = "Les deux mots de passes ne sont pas identiques";
         }
 
-        if (empty($message) && empty($nameErr) && empty($usernameErr) && empty($emailErr) && empty($passwordErr) && empty($passwordConfirmationErr)){
+        if (empty($message) && empty($nameErr) && empty($usernameErr) && empty($birthdayErr) && empty($emailErr) && empty($passwordErr) && empty($passwordConfirmationErr)){
 
             $user = new Users();
             $userArray = $user->createUserArray($fname, $lname, $birthday, $address, $sexe,$username, $password, $email);
@@ -127,15 +151,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			<fieldset>
 				<legend>La lecture d’un roman jette sur la vie une lumière</legend>
 				<table class="tab-insc" cellpadding="10" cellspacing="5">
-					<tr> <td><label for="fname">First name:</label> </td> <td><input type="text" id="fname" name="fname" maxlength="100"  value="<?php echo htmlentities($fname);?>" required /> <span class="error"> <?php echo $fname;?></span> </td> </tr>
-					<tr><td><label for="lname">Last name:</label></td> <td><input type="text" id="lname" name="lname" maxlength="100"  value="<?php echo htmlentities($lname);?>" required /> <span class="error"> <?php echo $lname;?></span> </td> </tr>
-					<tr><td><label for="email">Email:</label></td><td><input type="email" id="email" name="email" value="<?php echo htmlentities($email);?>" required /> <span class="error"> <?php echo $email;?></span></td></tr>
-					<tr><td><label for="birthday">Birthday:</label> </td> <td> <input type="date" id="birthday" name="birthday" value="<?php echo htmlentities($birthday);?>" required /> <span class="error"> <?php echo $birthday;?></span> </td></tr>
+					<tr> <td><label for="fname">First name:</label> </td> <td><input type="text" id="fname" name="fname" maxlength="100"  value="<?php echo htmlentities($fname);?>" required /> <span class="error"> <?php echo $fnameErr;?></span> </td> </tr>
+					<tr><td><label for="lname">Last name:</label></td> <td><input type="text" id="lname" name="lname" maxlength="100"  value="<?php echo htmlentities($lname);?>" required /> <span class="error"> <?php echo $lnameErr;?></span> </td> </tr>
+					<tr><td><label for="email">Email:</label></td><td><input type="email" id="email" name="email" value="<?php echo htmlentities($email);?>" required /> <span class="error"> <?php echo $emailErr;?></span></td></tr>
+					<tr><td><label for="birthday">Birthday:</label> </td> <td> <input type="date" id="birthday" name="birthday" value="<?php echo htmlentities($birthday);?>" required /> <span class="error"> <?php echo $birthdayErr;?></span> </td></tr>
 					<tr> <td><label for="sexe">Sexe:</label> </td> <td> <select  name="sexe" id="sexe"> <option value="masculin"> Masculin</option>  <option value="feminin"> Feminin </option></select> </td></tr>
-					<tr><td><label for="adress">Address: </label> </td> <td> <input type="text" id="address" name="address" maxlength="150"  value="<?php echo htmlentities($address);?>" required /> <span class="error"> <?php echo $address;?></span> </td></tr>
-					<tr><td><label for="username">Username : </label> </td> <td> <input type="text" id="username" name="username" maxlength="30"  value="<?php echo htmlentities($username);?>" required /> <span class="error"> <?php echo $username;?></span></td></tr>
-					<tr><td><label for="password">Password:</label> </td> <td> <input type="password" id="password" name="password" maxlength="30"  value="<?php echo htmlentities($password);?>" required /> <span class="error"> <?php echo $password;?></span> </td></tr>
-					<tr><td><label for="password-confirmation">Password-confirmation:</label> </td> <td> <input type="password" id="password-confirmation" name="password-confirmation"value="<?php echo htmlentities($passwordConfirmation);?>" required /> <span class="error"> <?php echo $passwordConfirmation;?></span></td></tr>
+					<tr><td><label for="adress">Address: </label> </td> <td> <input type="text" id="address" name="address" maxlength="150"  value="<?php echo htmlentities($address);?>" required /> <span class="error"> <?php echo $addressEr;?></span> </td></tr>
+					<tr><td><label for="username">Username : </label> </td> <td> <input type="text" id="username" name="username" maxlength="30"  value="<?php echo htmlentities($username);?>" required /> <span class="error"> <?php echo $usernameErr;?></span></td></tr>
+					<tr><td><label for="password">Password:</label> </td> <td> <input type="password" id="password" name="password" maxlength="30"  value="<?php echo htmlentities($password);?>" required /> <span class="error"> <?php echo $passwordErr;?></span> </td></tr>
+					<tr><td><label for="password-confirmation">Password-confirmation:</label> </td> <td> <input type="password" id="password-confirmation" name="password-confirmation"value="<?php echo htmlentities($passwordConfirmation);?>" required /> <span class="error"> <?php echo $passwordConfirmationErr;?></span></td></tr>
 				</table>
 				<div class="btn-insc">
 					<button  class="btn btn-clean" type="submit" name="submit"> s'inscrire </button> <br> <br>
